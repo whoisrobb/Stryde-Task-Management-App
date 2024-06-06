9e07419fcb346992cbf4d27c738443599b5bbf95
Muchiri <developedbyrobbie@gmail.com>
2024-06-06 22:59:48 +0300

    fix: Fix layout shifts, scrollbar

--- client/src/app/workspace/layout.tsx
+++ client/src/app/workspace/layout.tsx
@@ -8,9 +8,9 @@ export default function WorkspaceLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <body className="flex h-screen">
+    <body className="flex h-screen max-w-screen">
       <Sidebar />
-      <div className="px-2 w-full">
+      <div className="px-2 lg:w-[calc(100vw-17.5rem)] w-screen">
         <Header />
         {children}
       </div>
